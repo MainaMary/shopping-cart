1ce513627bbbcb096f04123301d7126e585d91d1
Mary <marieshix98@gmail.com>
2022-11-04 21:58:18 +0300

    Update the unique identifier

--- backend/products.js
+++ backend/products.js
@@ -1,6 +1,6 @@
 const products = [
   {
-    id: 1,
+    id: Math.floor(Math.random() * 1000),
     name: "Axe body wash",
     description:
       "An iconic combo of fresh sage & cedarwood scent with long-lasting odor protection",
@@ -9,7 +9,7 @@ const products = [
       "https://res.cloudinary.com/dvuszoody/image/upload/v1663774015/shopping-spree/bodyWash_qvrcf7.png",
   },
   {
-    id: 2,
+    id: Math.floor(Math.random() * 100),
     name: "Cleo soap",
     description:
       "An iconic combo of fresh sage & cedarwood scent with long-lasting odor protection",
@@ -18,7 +18,7 @@ const products = [
       "https://res.cloudinary.com/dvuszoody/image/upload/v1663774010/shopping-spree/soap_dkx5pa.jpg",
   },
   {
-    id: 3,
+    id: Math.floor(Math.random() * 100),
     name: "Bio-oil lotion",
     description:
       "Bio oil body Lotion pioneers a new level of lightness and efficacy in body moisturisation",
@@ -27,7 +27,43 @@ const products = [
       "https://res.cloudinary.com/dvuszoody/image/upload/v1663774010/shopping-spree/bio-oil_owp2nf.jpg",
   },
   {
-    id: 4,
+    id: Math.floor(Math.random() * 100),
+    name: "Cetaphil oily skin cleanser",
+    description:
+      "The Cetaphil restoraderm body wash is formulated to soothe dry, sensitive skin",
+    price: "270",
+    image:
+      "https://res.cloudinary.com/dvuszoody/image/upload/v1663774010/shopping-spree/cleanser_nxjwoe.jpg",
+  },
+  {
+    id: Math.floor(Math.random() * 100),
+    name: "Bio-oil lotion",
+    description:
+      "Bio oil body Lotion pioneers a new level of lightness and efficacy in body moisturisation",
+    price: "450",
+    image:
+      "https://res.cloudinary.com/dvuszoody/image/upload/v1663774010/shopping-spree/bio-oil_owp2nf.jpg",
+  },
+  {
+    id: Math.floor(Math.random() * 100),
+    name: "Cetaphil oily skin cleanser",
+    description:
+      "The Cetaphil restoraderm body wash is formulated to soothe dry, sensitive skin",
+    price: "270",
+    image:
+      "https://res.cloudinary.com/dvuszoody/image/upload/v1663774010/shopping-spree/cleanser_nxjwoe.jpg",
+  },
+  {
+    id: Math.floor(Math.random() * 100),
+    name: "Bio-oil lotion",
+    description:
+      "Bio oil body Lotion pioneers a new level of lightness and efficacy in body moisturisation",
+    price: "450",
+    image:
+      "https://res.cloudinary.com/dvuszoody/image/upload/v1663774010/shopping-spree/bio-oil_owp2nf.jpg",
+  },
+  {
+    id: Math.floor(Math.random() * 100),
     name: "Cetaphil oily skin cleanser",
     description:
       "The Cetaphil restoraderm body wash is formulated to soothe dry, sensitive skin",
