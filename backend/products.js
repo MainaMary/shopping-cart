ff666c772101bed4fe2026c2beff02b95ba6b3e6
Mary <marieshix98@gmail.com>
2022-11-17 16:36:06 +0300

    Upate the image urls from cloudinary

--- backend/products.js
+++ backend/products.js
@@ -37,39 +37,39 @@ const products = [
   },
   {
     id: Math.floor(Math.random() * 100),
-    name: "Bio-oil lotion",
+    name: "Portable cups and plates",
     description:
       "Bio oil body Lotion pioneers a new level of lightness and efficacy in body moisturisation",
     price: "$45",
     image:
-      "https://res.cloudinary.com/dvuszoody/image/upload/v1663774010/shopping-spree/bio-oil_owp2nf.jpg",
+      "https://res.cloudinary.com/kenyatta-university/image/upload/v1668690506/shopping%20cart/cupsplates_z7azqb.jpg",
   },
   {
     id: Math.floor(Math.random() * 100),
-    name: "Cetaphil oily skin cleanser",
+    name: "Ladies bags",
     description:
       "The Cetaphil restoraderm body wash is formulated to soothe dry, sensitive skin",
     price: "$27",
     image:
-      "https://res.cloudinary.com/dvuszoody/image/upload/v1663774010/shopping-spree/cleanser_nxjwoe.jpg",
+      "https://res.cloudinary.com/kenyatta-university/image/upload/v1668689517/shopping%20cart/ladiesbags_pijne8.jpg",
   },
   {
     id: Math.floor(Math.random() * 100),
-    name: "Bio-oil lotion",
+    name: "Shoes",
     description:
       "Bio oil body Lotion pioneers a new level of lightness and efficacy in body moisturisation",
     price: "$45",
     image:
-      "https://res.cloudinary.com/dvuszoody/image/upload/v1663774010/shopping-spree/bio-oil_owp2nf.jpg",
+      "https://res.cloudinary.com/kenyatta-university/image/upload/v1668690284/shopping%20cart/nikeshoes_cvytig.jpg",
   },
   {
     id: Math.floor(Math.random() * 100),
-    name: "Cetaphil oily skin cleanser",
+    name: "Necklace",
     description:
       "The Cetaphil restoraderm body wash is formulated to soothe dry, sensitive skin",
     price: "$27",
     image:
-      "https://res.cloudinary.com/dvuszoody/image/upload/v1663774010/shopping-spree/cleanser_nxjwoe.jpg",
+      "https://res.cloudinary.com/kenyatta-university/image/upload/v1668689545/shopping%20cart/necklace_qtxdmx.jpg",
   },
 ];
 module.exports = products;
